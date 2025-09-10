789c29c18bbe2dbbf02bfc06407a27ca74b25a59
Caroline Gao <caroline.x.gao@gmail.com>
2025-09-10 10:08:44 +1000

    Check and verify all image paths

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -55,7 +55,7 @@ const AboutSection = () => {
           </div>
           <div className="relative">
             <img 
-             src="/Image1.png"
+             src="/Images/Image1.png"
               alt="Youth collaboration and climate resilience" 
               className="w-full h-64 lg:h-full object-cover rounded-xl shadow-lg"
             />
