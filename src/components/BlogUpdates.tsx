b0396021561978be61d7cd94549534427a1c1a7d
Caroline Gao <caroline.x.gao@gmail.com>
2025-10-30 14:26:34 +1100

    Fix missing Link import in BlogUpdates component

--- src/components/BlogUpdates.tsx
+++ src/components/BlogUpdates.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Calendar, ArrowRight, X } from 'lucide-react';
 import { getLatestPosts, BlogPost } from '../data/blogPosts';
 
